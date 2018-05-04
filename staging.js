d0e861d14bba67d2e514c39bd4b34b8621b0b4df
sisou <hello@soerenschwert.de>
2018-05-04 21:43:48 +0200

    Format thousands in balances

--- staging.js
+++ staging.js
@@ -39,12 +39,28 @@ function _detectHashFormat(value) {
 }
 
 function _formatBalance(value) {
-    // If the value has no decimal places below 0.01, display 0 decimals
+    var valueStr = '';
+
+    // If the value has no decimal places below 0.01, display 2 decimals
     if(parseFloat(value.toFixed(2)) === value) {
-        return value.toFixed(2);
+        valueStr = value.toFixed(2);
     }
     // Otherwise, all required decimals will be displayed automatically
-    else return value;
+    else valueStr = value.toString();
+
+    var ints = _formatThousands(valueStr.split('.')[0]);
+    var decs = valueStr.split('.')[1];
+
+    return ints + '.' + decs;
+}
+
+function _formatThousands(number, separator) {
+    separator = separator || '\'';
+    let reversed = number.split('').reverse();
+    for(let i = 3; i < reversed.length; i += 4) {
+        reversed.splice(i, 0, separator);
+    }
+    return reversed.reverse().join('');
 }
 
 function _formatSize(size) {
