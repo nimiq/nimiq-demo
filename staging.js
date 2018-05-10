d67d0ee50f4ced2b5cbcf200e2ea5bcd69beafac
sisou <hello@soerenschwert.de>
2018-05-10 11:45:54 +0200

    Format tx data as UTF8 string

--- staging.js
+++ staging.js
@@ -73,6 +73,35 @@ function _formatSize(size) {
     }
 }
 
+// From https://github.com/nimiq/secure-utils/utf8-tools/utf8-tools.js
+function _formatTxData(bytes) {
+    // TODO: Use native implementations if/when available
+    var out = [], pos = 0, c = 0;
+    while (pos < bytes.length) {
+        var c1 = bytes[pos++];
+        if (c1 < 128) {
+            out[c++] = String.fromCharCode(c1);
+        } else if (c1 > 191 && c1 < 224) {
+            var c2 = bytes[pos++];
+            out[c++] = String.fromCharCode((c1 & 31) << 6 | c2 & 63);
+        } else if (c1 > 239 && c1 < 365) {
+            // Surrogate Pair
+            var c2 = bytes[pos++];
+            var c3 = bytes[pos++];
+            var c4 = bytes[pos++];
+            var u = ((c1 & 7) << 18 | (c2 & 63) << 12 | (c3 & 63) << 6 | c4 & 63) - 0x10000;
+            out[c++] = String.fromCharCode(0xD800 + (u >> 10));
+            out[c++] = String.fromCharCode(0xDC00 + (u & 1023));
+        } else {
+            var c2 = bytes[pos++];
+            var c3 = bytes[pos++];
+            out[c++] =
+            String.fromCharCode((c1 & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
+        }
+    }
+    return out.join('');
+}
+
 function _getAccountInfo(address, callback) {
     address = address.replace(/ /g, '+');
     fetch(apiUrl + '/account/' + address).then(function(response) {
